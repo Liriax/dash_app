674a3b868ac49258249ecacbe6b8bca03a6660bd
Liria <liriaxiaoge@gmail.com>
2021-04-18 14:21:06 +0200

    changed type of prodFeat and treeMatchAlgo to int, import classes

--- ParamsForCalc.py
+++ ParamsForCalc.py
@@ -1,18 +1,13 @@
+import pandas as pd
+
 class ParamsForCalc:
     # T: depreciation period = use time
-
     # r: interest rate
-
     # c_main: maintenance cost rate
-
     # c_person: hourly personnel cost rate
-
-    # t: work preparation time (hour)
-    # t_S: time saving
-    # t_t: throughput time
     # r_acc: revenue per product variant
 
-    def __init__(self, t_t, t_S, t):
+    def __init__(self):# t: work preparation time (hour); t_S: time saving; t_t: throughput time
         try:
             invest_params = pd.read_csv('parameter_Investitionsrechnung.csv')
             I_l2 = invest_params.iloc[0]["I_l2"]
@@ -25,19 +20,19 @@ class ParamsForCalc:
             T = invest_params.iloc[0]["t"]
             r_acc = invest_params.iloc[0]["npvRevProProduct"]
             self.I_total=I_l2+I_l3+I_al+I_pr 
-            self.S_person = c_person * t_S # personnel cost savings
             self.r=r
             self.C_depr = I_total/T
             self.C_int = 0.5*(I_total)*r
             self.C_main = c_main * I_total
-            self.C_person = c_person * t
-            self.R_acc = r_acc*t_S/t_t # additional revenues
         except pd.errors.EmptyDataError:
             invest_params = 'could not read parameter_Investitionsrechnung.csv'
 
         
 
-    def npv (self):
+    def npv (self, t_t, t_S, t):
+        S_person = c_person * t_S # personnel cost savings
+        C_person = c_person * t
+        R_acc = r_acc*t_S/t_t # additional revenues
         npv= - I_total
         for t in range (1,T+1):
             npv+=(R_acc+S_person-C_main)/(1+r)**t
--- calculator.py
+++ calculator.py
@@ -1,29 +1,29 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+import CurrentSituation
+import ParamsForCalc
 import pandas as pd
-from dash.dependencies import Input, Output, State
 
 
-def retrieve_ist_situation():
-    try:
-        ist_situation = pd.read_csv('ist_situation.csv')
-    except pd.errors.EmptyDataError:
-        ist_situation = 'could not read ist_situation.csv!'
-    return ist_situation
+def retrieve_ist_situation(): # the read, try and except are done by creating a CurrentSituation object
+    # try:
+    #     ist_situation = pd.read_csv('ist_situation.csv')
+    # except pd.errors.EmptyDataError:
+    #     ist_situation = 'could not read ist_situation.csv!'
+    # return ist_situation
+    return CurrentSituation()
 
 
-def retrieve_invest_params():
-    try:
-        invest_params = pd.read_csv('parameter_Investitionsrechnung.csv')
-    except pd.errors.EmptyDataError:
-        invest_params = 'could not read parameter_Investitionsrechnung.csv'
-    return invest_params
+def retrieve_invest_params(): # the read, try and except are done by creating a ParamsForCalc object
+    # try:
+    #     invest_params = pd.read_csv('parameter_Investitionsrechnung.csv')
+    # except pd.errors.EmptyDataError:
+    #     invest_params = 'could not read parameter_Investitionsrechnung.csv'
+    # return invest_params
+    return ParamsForCalc()
 
 
 def calculate_total_time(alternative, ist_situation):
     new_time = 0
-    if(ist_situation.treeMatAlgo == #f):
+    if(ist_situation.treeMatAlgo == 0): #treeMatAlgo is 0 when it's not used, otherwise it would be 1
         new_time = ist_situation.timeNewComponent
     else:
         new_time += 0
@@ -31,18 +31,18 @@ def calculate_total_time(alternative, ist_situation):
 
 
 class Calculator:
-    alternative1 = {"treeMatchAlgo": "false", "prodFeat": "false", "matLevel": "1"}
-    alternative2 = {"treeMatchAlgo": "false", "prodFeat": "true", "matLevel": "1"}
-    alternative3 = {"treeMatchAlgo": "true", "prodFeat": "false", "matLevel": "1"}
-    alternative4 = {"treeMatchAlgo": "true", "prodFeat": "true", "matLevel": "1"}
-    alternative5 = {"treeMatchAlgo": "false", "prodFeat": "false", "matLevel": "2"}
-    alternative6 = {"treeMatchAlgo": "false", "prodFeat": "true", "matLevel": "2"}
-    alternative7 = {"treeMatchAlgo": "true", "prodFeat": "false", "matLevel": "2"}
-    alternative8 = {"treeMatchAlgo": "true", "prodFeat": "true", "matLevel": "2"}
-    alternative9 = {"treeMatchAlgo": "false", "prodFeat": "false", "matLevel": "3"}
-    alternative10 = {"treeMatchAlgo": "false", "prodFeat": "true", "matLevel": "3"}
-    alternative11 = {"treeMatchAlgo": "true", "prodFeat": "false", "matLevel": "3"}
-    alternative12 = {"treeMatchAlgo": "true", "prodFeat": "true", "matLevel": "3"}
+    alternative1 = {"treeMatchAlgo": 0, "prodFeat": 0, "matLevel": "1"}
+    alternative2 = {"treeMatchAlgo": 0, "prodFeat": 1, "matLevel": "1"}
+    alternative3 = {"treeMatchAlgo": 1, "prodFeat": 0, "matLevel": "1"}
+    alternative4 = {"treeMatchAlgo": 1, "prodFeat": 1, "matLevel": "1"}
+    alternative5 = {"treeMatchAlgo": 0, "prodFeat": 0, "matLevel": "2"}
+    alternative6 = {"treeMatchAlgo": 0, "prodFeat": 1, "matLevel": "2"}
+    alternative7 = {"treeMatchAlgo": 1, "prodFeat": 0, "matLevel": "2"}
+    alternative8 = {"treeMatchAlgo": 1, "prodFeat": 1, "matLevel": "2"}
+    alternative9 = {"treeMatchAlgo": 0, "prodFeat": 0, "matLevel": "3"}
+    alternative10 = {"treeMatchAlgo": 0, "prodFeat": 1, "matLevel": "3"}
+    alternative11 = {"treeMatchAlgo": 1, "prodFeat": 0, "matLevel": "3"}
+    alternative12 = {"treeMatchAlgo": 1, "prodFeat": 1, "matLevel": "3"}
     alternativen = [alternative1, alternative2, alternative3, alternative4, alternative5, alternative6, alternative7,
                     alternative8, alternative9, alternative10, alternative11, alternative12]
 
